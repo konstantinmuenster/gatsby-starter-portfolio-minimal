9fb20a16d3d703ff914ba93e247ae6b66bcecb9d
Konstantin Münster <konstantin.muenster@mail.com>
2020-12-05 17:28:35 +0100

    Update gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,70 @@
+const {
+  author,
+  siteTitle,
+  siteShortTitle,
+  siteDescription,
+  siteIcon,
+  siteUrl,
+  colors,
+} = require("./config")
+
+module.exports = {
+  siteMetadata: {
+    author: author,
+    title: siteTitle,
+    description: siteDescription,
+    siteUrl: siteUrl,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    // {
+    //   resolve: `gatsby-plugin-gtag`,
+    //   options: {
+    //     trackingId: `UA-XXXXXXXX-X`,
+    //     head: false,
+    //     anonymize: true,
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: siteTitle,
+        short_name: siteShortTitle,
+        start_url: `/`,
+        background_color: colors.lightTheme.background,
+        theme_color: colors.lightTheme.primary,
+        display: `minimal-ui`,
+        icon: siteIcon, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1000,
+              quality: 80,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content`,
+        name: `content`,
+      },
+    },
+  ],
+}
