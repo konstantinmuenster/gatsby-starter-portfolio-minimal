9027fab00382104a891b53444c4b70b2b67d400c
Konstantin Münster <konstantin.muenster@mail.com>
2020-12-05 06:59:48 +0100

    Add theme colors and fonts to config file

--- config/index.js
+++ config/index.js
@@ -1,71 +1,88 @@
-module.exports = {
+export default {
+  author: "@konstantinmuenster",
+  siteTitle: "Gatsby Starter Portfolio Minimal",
+  siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
+  siteDescription:
+    "A modern one-page portfolio with a clean yet expressive design.",
+  siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+  siteLanguage: "en_US",
+  siteIcon: "content/favicon.png", // Relative to gatsby-config file
+  seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
 
-    author: "@konstantinmuenster",
-    siteTitle: "Gatsby Starter Portfolio Minimal",
-    siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
-    siteDescription: "A modern one-page portfolio with a clean yet expressive design.",
-    siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
-    siteLanguage: "en_US",
-    siteIcon: "content/favicon.png", // Relative to gatsby-config file
-    seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  // Set this to true if you want to use the splash screen
+  splashScreen: false,
 
-    splashScreen: false, // Set this to true if you want to use the splash screen
+  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+  mediumRssFeed:
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
+  shownArticles: 3,
 
-    // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
-    // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
-    shownArticles: 3,
-    
-    // There are icons available for the following platforms: 
-    // Medium, GitHub, LinkedIn, XING, Behance
-    socialMedia: [
-        {
-            name: "Linkedin",
-            url: "https://www.linkedin.com/in/konstantin-muenster/"
-        },
-        {
-            name: "Medium",
-            url: "https://konstantinmuenster.medium.com/"
-        },
-        {
-            name: "Github",
-            url: "https://github.com/konstantinmuenster/gatsby-starter-portfolio-minimal"
-        },
-        {
-            name: "Behance",
-            url: "https://www.behance.net/konstanmnster"
-        },
-    ],
-  
-    navLinks: {
-        menu: [
-            {
-                name: "Articles",
-                url: "/#articles",
-            },
-            {
-                name: "About Me",
-                url: "/#about",
-            },
-            {
-                name: "Features",
-                url: "/#projects",
-            },
-        ],
-        button: {
-            name: "Contact",
-            url: "/#contact",
-        }
+  // Theme colors
+  colors: {
+    primary: "#000000",
+    secondary: "#FFF4D9",
+    tertiary: "#F2F2F2",
+    text: "#000000",
+    background: "#ffffff",
+  },
+
+  // Theme fonts (if you use other fonts, make sure you include the font file too)
+  fonts: {
+    primary: "Roboto, Arial, sans-serif",
+  },
+
+  // There are icons available for the following platforms:
+  // Medium, GitHub, LinkedIn, XING, Behance
+  socialMedia: [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/konstantin-muenster/",
     },
+    {
+      name: "Medium",
+      url: "https://konstantinmuenster.medium.com/",
+    },
+    {
+      name: "Github",
+      url:
+        "https://github.com/konstantinmuenster/gatsby-starter-portfolio-minimal",
+    },
+    {
+      name: "Behance",
+      url: "https://www.behance.net/konstanmnster",
+    },
+  ],
 
-    footerLinks: [
-        {
-            name: "Privacy",
-            url: "/privacy"
-        },
-        {
-            name: "Imprint",
-            url: "/imprint"
-        }
-    ]
+  navLinks: {
+    menu: [
+      {
+        name: "Articles",
+        url: "/#articles",
+      },
+      {
+        name: "About Me",
+        url: "/#about",
+      },
+      {
+        name: "Features",
+        url: "/#projects",
+      },
+    ],
+    button: {
+      name: "Contact",
+      url: "/#contact",
+    },
+  },
+
+  footerLinks: [
+    {
+      name: "Privacy",
+      url: "/privacy",
+    },
+    {
+      name: "Imprint",
+      url: "/imprint",
+    },
+  ],
 }
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,19 +1,15 @@
-module.exports = {
-  colors: {
-    primary: "#000000",
-    secondary: "#FFF4D9",
-    tertiary: "#F2F2F2",
-    text: "#000000",
-    background: "#ffffff",
-  },
+import config from "../../config"
+
+const { colors, fonts } = config
+
+export default {
+  colors: colors,
+  fonts: fonts,
   breakpoints: {
     xs: "480px",
     sm: "768px",
     md: "992px",
     lg: "1200px",
-  },
-  fonts: {
-    primary: "Roboto, Arial, sans-serif",
   },
   borderRadius: "1rem",
   pageWidth: "62.5rem",
