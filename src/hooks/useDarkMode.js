060bcc4ed69774fd174b906f4e9e9da72f57523a
Konstantin Münster <konstantin.muenster@mail.com>
2021-03-18 20:27:57 +0100

    fix: site crashes in older Safari

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -6,7 +6,7 @@ const useDarkMode = () => {
   const { state, setState } = useContext(Context)
 
   useEffect(() => {
-    const darkScheme = "(prefers-color-scheme: dark)"
+    const darkSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)")
 
     const toggleTheme = event => {
       if (event.matches) {
@@ -16,14 +16,22 @@ const useDarkMode = () => {
       }
     }
 
-    if (window.matchMedia && window.matchMedia(darkScheme).matches) {
+    if (darkSchemeQuery) {
       setState({ ...state, darkMode: true })
     }
 
-    window.matchMedia(darkScheme).addEventListener("change", toggleTheme)
-
-    return () => {
-      window.matchMedia(darkScheme).removeEventListener("change", toggleTheme)
+    if (darkSchemeQuery.addEventListener) {
+      darkSchemeQuery.addEventListener("change", toggleTheme)
+      return () => {
+        darkSchemeQuery.removeEventListener("change", toggleTheme)
+      }
+    } else {
+      // backwards compatibility
+      // https://betterprogramming.pub/using-window-matchmedia-in-react-8116eada2588
+      darkSchemeQuery.addListener(toggleTheme)
+      return () => {
+        darkSchemeQuery.removeListener(toggleTheme)
+      }
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
